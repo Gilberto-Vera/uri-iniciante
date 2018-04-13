054076258724566df94d7b59165ca5bb5b599eef
gilberto.vera <gilberto.vera@tse.jus.br>
2018-04-13 16:04:36 -0300

    Complemeta comentário

--- 1018_Cedu/src/Main.java
+++ 1018_Cedu/src/Main.java
@@ -1,14 +1,16 @@
-
 import java.util.Scanner;
-
 /*
- * O código lê um valor inteiro e calcula o menor número de notas possíveis 
- * (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de
- * 100, 50, 20, 10, 5, 2 e 1 e mostra o valor lido e a relação de notas
- * necessárias.
- *
+ * Desafio: 1018 Cédulas
+ * Descrição: O código lê um valor inteiro e calcula o menor número de notas
+ * possíveis (cédulas) no qual o valor pode ser decomposto. As notas
+ * consideradas são de 100, 50, 20, 10, 5, 2 e 1 e mostra o valor lido e a
+ * relação de notas necessárias.
+ */
+
+/**
  * @author gilberto
- * @date 2018-04-04
+ * @since 2018-04-04
+ * @version 0.1
  */
 public class Main {
     
