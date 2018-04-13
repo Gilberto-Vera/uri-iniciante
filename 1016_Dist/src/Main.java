00803823b34f68671de61c1eb99213fe73638437
gilberto.vera <gilberto.vera@tse.jus.br>
2018-04-13 16:04:21 -0300

    Complemeta comentário

--- 1016_Dist/src/Main.java
+++ 1016_Dist/src/Main.java
@@ -1,9 +1,8 @@
-
 import java.util.Scanner;
-
 /*
- * O código lê a distância (em Km) e calcula quanto tempo leva (em minutos)
- * para o carro Y tomar essa distância do outro carro.
+ * Desafio: 1016 Distância
+ * Descrição: O código lê a distância (em Km) e calcula quanto tempo leva (em
+ * minutos) para o carro Y tomar essa distância do outro carro.
  * 1 - Dois carros (x e y) partem para numa mesma direção.
  * 2 - x sai com uma velocidade constante de 60 km/h.
  * 3 - y sai com uma velocidade constante de 90 km/h.
@@ -11,9 +10,12 @@ import java.util.Scanner;
  * 5 - Tempo = (distância / velocidade relativa) * 60 = (distância / 30) * 60 =
  * distância * 2.
  * 6 - Converter o tempo de hora para minuto multiplicando o resultado por 60.
- * 
+ */
+
+/**
  * @author gilberto
- * 20/03/2018
+ * @since 2018-03-20
+ * @version 0.1
  */
 public class Main {
     public static void main(String args[]){
