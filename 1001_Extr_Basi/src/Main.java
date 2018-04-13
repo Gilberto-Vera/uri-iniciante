99436d6a634387b0f1f1c95bc1ece83ab73d06d7
gilberto.vera <gilberto.vera@tse.jus.br>
2018-04-13 16:02:13 -0300

    Complemeta comentário

--- 1001_Extr_Basi/src/Main.java
+++ 1001_Extr_Basi/src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-
- /* Desafio 1001 Extramamente Básico
- * lê dois valores e apresenta a soma desses valores
+/* 
+ * Desafio: 1001 Extramamente Básico
+ * Descrição: O código lê dois valores e apresenta a soma desses valores
  */
 
- /**
+/**
  * @author gilberto
  * @since 2018-03-09
- * @version 1.0
+ * @version 0.1
  */
 
 public class Main {
