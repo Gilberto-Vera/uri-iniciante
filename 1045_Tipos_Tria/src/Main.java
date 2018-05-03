c03ebbea67b41dbdf2ccb622f2f636b66d05dd27
gilberto.vera <gilberto.vera@tse.jus.br>
2018-05-03 20:02:07 -0300

    Desafio 1045

---
+++ 1045_Tipos_Tria/src/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * Desafio: 1045 Tipos de Triângulos
+ * Descrição: O código lê 3 valores de ponto flutuante A, B e C e ordena-os em
+ * ordem decrescente, de modo que o lado A representa o maior dos 3 lados. A
+ * seguir, determina o tipo de triângulo que estes três lados formam, com base
+ * nos seguintes casos, sempre escrevendo uma mensagem adequada:
+ *
+ *   se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
+ *   se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
+ *   se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
+ *   se A2 < B2 + C2, apresente a mensagem: TRIANGULO ACUTANGULO
+ *   se os três lados forem iguais, apresente a mensagem: TRIANGULO EQUILATERO
+ *   se apenas dois dos lados forem iguais, apresente a mensagem: TRIANGULO
+ * ISOSCELES
+ */
+
+/**
+ * @author gilberto
+ * @since 2018-05-03
+ * @version 0.1
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        int a = ler.nextInt();
+        int b = ler.nextInt();
+        int c = ler.nextInt();
+        
+        
+    }
+}
